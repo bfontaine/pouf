a30836c052746d670f78b797e495f18dca1bbcf2
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-02 23:01:19 +0100

    some tests

--- tests/tests.rb
+++ tests/tests.rb
@@ -26,6 +26,47 @@ class PoufTests < Test::Unit::TestCase
     assert(Pouf.version =~ /^\d+\.\d+\.\d+/)
   end
 
+  # == Pouf#play_cmd == #
+
+  def test_pouf_play_cmd_env_no_spaces
+    s = 'some_random_command'
+    ENV['POUF_CMD'] = s
+    assert_equal([s], Pouf.play_cmd)
+  end
+
+  def test_pouf_play_cmd_env_with_spaces
+    s = 'some random command'
+    ENV['POUF_CMD'] = s
+    assert_equal(%w[some random command], Pouf.play_cmd)
+  end
+
+  # == Pouf#filename2alias == #
+
+  def test_pouf_filename2alias_empty
+    assert_nil(Pouf.filename2alias(''))
+  end
+
+  def test_pouf_filename2alias_no_path
+    n = 'foobar'
+    assert_equal(n, Pouf.filename2alias("#{n}.ext"))
+  end
+
+  def test_pouf_filename2alias_with_dirs
+    n = 'foobar'
+    assert_equal(n, Pouf.filename2alias("foo/bar/#{n}.ext"))
+  end
+
+  def test_pouf_filename2alias_absolute_path
+    n = 'foobar'
+    assert_equal(n, Pouf.filename2alias("/foo/bar/#{n}.ext"))
+  end
+
+  def test_pouf_filename2alias_relative_path
+    n = 'foobar'
+    assert_equal(n, Pouf.filename2alias("../bar/#{n}.ext"))
+    assert_equal(n, Pouf.filename2alias("./bar/#{n}.ext"))
+  end
+
 end
 
 
