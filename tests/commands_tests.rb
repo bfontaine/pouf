99c15fe6faa39474f226898b102e0bc36f7aa06e
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-05 12:51:39 +0100

    more tests regarding files management

---
+++ tests/commands_tests.rb
@@ -0,0 +1,52 @@
+# -*- coding: UTF-8 -*-
+
+require 'tmpdir'
+require 'fileutils'
+
+class PoufCommandsTests < Test::Unit::TestCase
+
+  def setup
+    @prev_cmd = ENV['POUF_CMD']
+    ENV['POUF_CMD'] = 'echo'
+    @prev_dir = Pouf.sounds_dir
+    Pouf.sounds_dir = @sounds_dir = Dir.mktmpdir('pouf-tests')
+  end
+
+  def teardown
+    Pouf.sounds_dir = @prev_dir
+    FileUtils.rm_rf @sounds_dir
+    ENV['POUF_CMD'] = @prev_cmd
+  end
+
+  # == Pouf#alias2filename == #
+  
+  def test_pouf_alias2filename_empty
+    assert_nil(Pouf.alias2filename('foo'))
+  end
+
+  def test_pouf_alias2filename_one_file_no_extension
+    f = "#{@sounds_dir}/foo"
+    FileUtils.touch f
+    assert_nil(Pouf.alias2filename('foo'))
+  end
+
+  def test_pouf_alias2filename_one_file_extension
+    f = "#{@sounds_dir}/foo.bar"
+    FileUtils.touch f
+    assert_equal(f, Pouf.alias2filename('foo'))
+  end
+
+  def test_pouf_alias2filename_one_file_prefix
+    f = "#{@sounds_dir}/foobar"
+    FileUtils.touch f
+    assert_nil(Pouf.alias2filename('foo'))
+  end
+
+  def test_pouf_alias2filename_two_files_same_basename
+    f = "#{@sounds_dir}/foo.bar"
+    FileUtils.touch f
+    FileUtils.touch "#{@sounds_dir}/foo.qux"
+    assert_equal(f, Pouf.alias2filename('foo'))
+  end
+
+end
